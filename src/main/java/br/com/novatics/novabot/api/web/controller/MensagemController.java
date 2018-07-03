921bd9369b89f213c89d1e775584342827dbe2bc
jopss <joao.paulo@metaverso.com.br>
2018-07-03 16:56:30 -0300

    melhorando controller

--- src/main/java/br/com/novatics/novabot/api/web/controller/MensagemController.java
+++ src/main/java/br/com/novatics/novabot/api/web/controller/MensagemController.java
@@ -5,28 +5,27 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.novatics.novabot.api.model.Mensagem;
 import br.com.novatics.novabot.api.model.service.MensagemService;
+import br.com.novatics.novabot.api.web.util.NovabotController;
 import br.com.novatics.novabot.api.web.util.Resposta;
 
 @RestController
-public class MensagemController {
+public class MensagemController extends NovabotController {
 
 	@Autowired
 	private MensagemService mensagemService;
 
-	@RequestMapping(
-        value = "/mensagem/salvar", method = RequestMethod.POST,
-        consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = MediaType.APPLICATION_JSON_VALUE
-    )
-	//@PostMapping(value = "/mensagem/salvar", produces = MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-	public @ResponseBody Resposta cadastrar(@RequestBody Mensagem mensagem) {
+//	@RequestMapping(
+//        value = "/mensagem/salvar", method = RequestMethod.POST,
+//        consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = MediaType.APPLICATION_JSON_VALUE
+//    )
+	@PostMapping(value = "/mensagem/salvar", produces = MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public Resposta cadastrar(@RequestBody Mensagem mensagem) {
 		mensagemService.salvar(mensagem);
 		return Resposta.sucesso().comDado(mensagem);
 	}
